d266ce9594eb68e32e7a26cbd153ae1d927fd548
Prashant C <pvc@outlook.com>
2016-10-07 16:34:07 +0530

    Updated the design files

--- WeatherApp/ViewController.designer.cs
+++ WeatherApp/ViewController.designer.cs
@@ -1,17 +1,38 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace WeatherApp
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Description { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        MapKit.MKMapView MyMap { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (Description != null) {
+                Description.Dispose ();
+                Description = null;
+            }
+
+            if (MyMap != null) {
+                MyMap.Dispose ();
+                MyMap = null;
+            }
         }
     }
 }
